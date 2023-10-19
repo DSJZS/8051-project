dab4d2cbae42cbcfb93a81ceb83341eeffa26c10
Cheng <3155414334@qq.com>
2023-10-19 11:56:45 +0800

    LCD1602调试工具

---
+++ 4.2LCD1602-Debug-Tool/main.c
@@ -0,0 +1,17 @@
+#include <REGX52.H>
+
+#include "LCD1602.h"
+#include "Delay.h"
+
+int result = 0;
+
+void main()
+{
+	LCD_Init();
+	
+	while(1)
+	{
+		LCD_ShowNum(1,1,result++,16);
+		Delay(1000);
+	}
+}
