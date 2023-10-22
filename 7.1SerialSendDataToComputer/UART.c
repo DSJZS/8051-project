9376be39bba5013360652a91a161c9a8a5ca9997
Cheng <3155414334@qq.com>
2023-10-22 15:53:05 +0800

    单片就通过串口发送数据给电脑

---
+++ 7.1SerialSendDataToComputer/UART.c
@@ -0,0 +1,32 @@
+#include "UART.h"
+
+/**
+  *@brief		串口初始化，4800bps@11.0592MHz
+  *@param		无
+  *@retval	无
+  **/
+void UART_Init(void)		
+{
+	PCON |= 0x80;		//使能波特率倍速位SMOD
+	SCON = 0x50;		//8位数据,可变波特率
+	TMOD &= 0x0F;		//设置定时器模式
+	TMOD |= 0x20;		//设置定时器模式
+	TL1 = 0xF4;			//设置定时初始值
+	TH1 = 0xF4;			//设置定时重载值
+	ET1 = 0;			//禁止定时器中断
+	TR1 = 1;			//定时器1开始计时
+}
+
+/**
+  *@brief		串口发送一字节数据
+  *@param		Byte 要发送的一个字节数据
+  *@retval	无
+  **/
+void UART_SendByte(unsigned char Byte)
+{
+	SBUF = Byte;	//	发送数据
+	
+	while(TI==0);	//	确保数据发送完全
+	
+	TI=0;
+}
